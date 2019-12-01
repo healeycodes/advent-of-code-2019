53fea83319f2d6f31dfdf76f984a04681afd0b33
Andrew Healey <34559231+healeycodes@users.noreply.github.com>
2019-12-01 17:48:58 +0000

    Better naming :)

--- day_one.py
+++ day_one.py
@@ -1,18 +1,18 @@
 import sys
 
-fn = sys.argv[1]
-with open(fn) as file:
-    answer = 0
+filename = sys.argv[1]
+with open(filename) as file:
+    total_fuel = 0
     for line in file.read().splitlines():
         mass = int(line)
         
         # calculate fuel (part one)
         fuel = mass // 3 - 2
-        answer += fuel
+        total_fuel += fuel
 
         # keep calculating fuel for the fuel until zero (part two)
         while fuel > 0:
-            fuel = max([0, fuel // 3 - 2])
-            answer += fuel
+            fuel = max((0, fuel // 3 - 2))
+            total_fuel += fuel
 
-    print(answer)
+    print(total_fuel)
