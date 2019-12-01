4fac3b54bfbcd95c0ff48aa0bfa73012900c8140
Andrew <healeycodes@gmail.com>
2019-12-01 18:19:53 +0000

    Split into part one/part two

---
+++ day_one/part_one.py
@@ -0,0 +1,13 @@
+import sys
+
+filename = sys.argv[1]
+with open(filename) as file:
+    total_fuel = 0
+    for line in file.read().splitlines():
+        mass = int(line)
+        
+        # calculate fuel (part one)
+        fuel = mass // 3 - 2
+        total_fuel += fuel
+
+    print(total_fuel)
